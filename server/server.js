481131044fafa85aa915d03c36c874fd55fa8497
Patrikas Karpickas <patrikas.karpickas@mif.stud.vu.lt>
2022-11-21 14:37:50 +0200

    Fixed missing file

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const dbi = require('./app/config/db.config')
+const multer = require('multer')
+const mysql = require('mysql2')
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 8080
+
+const upload = multer({
+    dest: './uploads',
+})
+
+const db = mysql.createConnection({
+    host: dbi.HOST,
+    user: dbi.USER,
+    password: dbi.PASSWORD,
+    database: dbi.DB,
+})
+db.connect(function (err) {
+    if (err) {
+        return console.error('error: ' + err.message)
+    }
+    console.log('Connected to the MySQL server.')
+})
+
+app.use(cors())
+app.use(express.json())
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    var imgsrc = 'http://localhost:8080/uploads/' + req.file.filename
+    var insertData = 'INSERT INTO images(file_src)VALUES(?)'
+    db.query(insertData, [imgsrc], (err, result) => {
+        if (err) throw err
+        console.log('file uploaded')
+    })
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
